90895d1729157b4a898d94c9a29ad38cfd531579
Mike Plexousakis <mike.plexousakis@gmail.com>
2017-01-10 09:57:52 +0200

    First commit
    First commit: perform a POST into torawallet-checkout - lookup,
    receive the results (JSON) and populate a form doing a POST
    in torawallet-checkout - verify targeting an iframe.

---
+++ test.php
@@ -0,0 +1,50 @@
+<?php
+       $httpContextOptions = [
+           'http' => [
+               'method' => 'POST',
+               'content' => json_encode([
+                  'card'  => [
+                    'number' => '5892420000000943',
+                    'name' => 'Mike Plexousakis',
+                    'cvv' => '347',
+                    'exp_date' => '01/20'
+                  ],
+                  'amount' => '1800',
+                  'pk' => 'pk_nknDsvb04gH36GTIO2Ho2n5VbUukkVTQ'	
+               ]),
+               'header' => 'Content-Type: application/json',
+           ]
+       ];
+       $uri = 'http://checkout.torawallet.local/threedsecure';
+
+       $context = stream_context_create($httpContextOptions);
+       $fp = fopen($uri, 'r', false, $context);
+       $response = stream_get_contents($fp);
+       fclose($fp);
+
+       $ra = json_decode($response, true);
+?>
+
+<html>
+<head>
+</head>
+<body>
+Will POST to <?= $ra['url'] ?>
+<br/><br/>
+<form method="POST" action="<?= $ra['url'] ?>" target="tds">
+<textarea rows="10" cols="80" name="PaReq"><?= $ra['pares'] ?></textarea>
+<br/><br/>
+<input style="width:500;" value="<?= $ra['term_url'] ?>" type="text" name="TermUrl"/>
+<br/><br/>
+<input style="width:500;" value="<?= $ra['md'] ?>" name="MD" type="text"/>
+<br/><br/>
+<input type="submit"/>
+</form>
+<iframe name="tds" id="tds" src="" width="500" height="500"></iframe>
+<script type="text/javascript">
+  window.addEventListener("message", function(e){
+	console.log(JSON.parse(e.data)); }, false);
+</script>
+</body>
+</html>
+
